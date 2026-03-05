d67fb822f82f6216909a9f89edf8261c2412f764
Sajjad Saffan <sajjadsaffan634@gmail.com>
2026-03-05 18:38:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4232362537-d9d12",
+  "appId": "1:849327218463:web:e10846027074a1f5b41a2a",
+  "apiKey": "AIzaSyDRpdtdDA86VTSXGZz0WDPl4b8fuIa8l8g",
+  "authDomain": "studio-4232362537-d9d12.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "849327218463"
+};
